89a19f9fb8de7c507bbfbc55ef175e62891d02be
camilogs <camilo.gonzalez.col@gmail.com>
2024-03-16 16:11:14 -0500

    chore(party.py): Se implementan estilos pep8

--- modules/optical_equipment/party.py
+++ modules/optical_equipment/party.py
@@ -2,24 +2,24 @@
 # this repository contains the full copyright notices and license terms.
 
 from trytond.pool import PoolMeta
-from trytond.model import ModelSQL, ModelView, fields
-from trytond.pyson import Eval, If
+from trytond.model import fields
+from trytond.pyson import Eval
 
-#from . import equipment
 
 _CUSTOMER_TYPE = [('ips', 'IPS'),
                 ('optica', 'Optica'),
                 ('otro', 'Otro')]
+
 
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
 
     customer_type = fields.Selection(_CUSTOMER_TYPE, "Customer Type")
 
-    
+
 class Address(metaclass=PoolMeta):
     __name__ = 'party.address'
 
     campus = fields.Boolean("Campus")
     party_related = fields.Many2One('party.party', "Party Related",
-                                    states ={ 'invisible': (~Eval("campus"))})
+                                    states={'invisible': (~Eval("campus"))})
